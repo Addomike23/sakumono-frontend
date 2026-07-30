f4493618ad22be3718ab24051a3bec3217cba08f
Addomike23 <addomichaelopare@gmail.com>
2026-07-30 05:02:07 +0000

    url

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "https://sakumono-backend.vercel.app/";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://sakumono-backend.vercel.app/api";
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
